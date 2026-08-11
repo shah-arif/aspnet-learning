6883192479e85f315b6ff72e1ec62d5aee251f61
shah-arif <shaharifabdullah@gmail.com>
2026-08-11 23:22:40 +0600

    added Day01 and  Day02

---
+++ Day01/Program.cs
@@ -0,0 +1,10 @@
+﻿string name = "Abdullah";
+int age = 29;
+string profession = "Software Engineer";
+string[] favouriteLanguage = { "C#", "Dart" };
+
+Console.WriteLine($"Hello {name}, you are {age} years old and you are a {profession}.");
+Console.WriteLine($"Your favourite programming languages are {string.Join(", ", favouriteLanguage)}.");
+
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
---
+++ Day02/Program.cs
@@ -0,0 +1,21 @@
+﻿string productName = "Burger";
+decimal price = 250M;
+int quantity = 3;
+
+decimal total = price * quantity;
+
+Console.WriteLine($"Product: {productName}");
+Console.WriteLine($"Price: {price}");
+Console.WriteLine($"Quantity: {quantity}");
+Console.WriteLine($"Total Price: {total}");
+
+if (quantity > 0)
+{
+    Console.WriteLine("Order can be placed.");
+}
+else
+{
+    Console.WriteLine("Order cannot be placed.");
+}
+
+Console.ReadKey();
